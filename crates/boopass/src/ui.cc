e52f57ac29628da21fad848f5e2de0d8babc8004
Deltara <50772027+Deltara3@users.noreply.github.com>
2026-03-23 01:38:31 -0400

    add(core): input support

--- crates/boopass/src/ui.cc
+++ crates/boopass/src/ui.cc
@@ -4,9 +4,14 @@
 #include "imgui_impl_dx10.h"
 #include "imgui_impl_win32.h"
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 static bool g_Initialized = false;
+static bool g_MenuShown = false;
 static ID3D10Device* g_Device = nullptr;
 static ID3D10RenderTargetView* g_RenderTarget = nullptr;
+static WNDPROC o_WndProc = nullptr;
 
 HRESULT WINAPI hk_Present(IDXGISwapChain* pSwapChain, UINT sync, UINT flags) {
     if (!g_Initialized) {
@@ -19,6 +24,8 @@ HRESULT WINAPI hk_Present(IDXGISwapChain* pSwapChain, UINT sync, UINT flags) {
         pSwapChain->GetBuffer(0, __uuidof(ID3D10Texture2D), (void**)&pBackBuffer);
         g_Device->CreateRenderTargetView(pBackBuffer, nullptr, &g_RenderTarget);
         pBackBuffer->Release();
+
+        o_WndProc = (WNDPROC)SetWindowLongPtr(desc.OutputWindow, GWLP_WNDPROC, (intptr_t)WndProc);
 
         ImGui::CreateContext();
         ImGui_ImplWin32_Init(desc.OutputWindow);
@@ -35,7 +42,17 @@ HRESULT WINAPI hk_Present(IDXGISwapChain* pSwapChain, UINT sync, UINT flags) {
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
 
-    ImGui::ShowDemoWindow();
+    if (g_MenuShown) {
+        while (ShowCursor(TRUE) < 0);
+
+        ImGui::Begin("Boopass");
+
+        // TODO: Everything between this.
+
+        ImGui::End();
+    } else {
+        while (ShowCursor(FALSE) >= 0);
+    }
 
     ImGui::EndFrame();
     ImGui::Render();
@@ -44,4 +61,13 @@ HRESULT WINAPI hk_Present(IDXGISwapChain* pSwapChain, UINT sync, UINT flags) {
     ImGui_ImplDX10_RenderDrawData(ImGui::GetDrawData());
 
     return o_Present(pSwapChain, sync, flags);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (uMsg == WM_KEYDOWN && wParam == VK_F12) {
+        g_MenuShown = !g_MenuShown;
+    }
+
+    ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
+    return CallWindowProc(o_WndProc, hWnd, uMsg, wParam, lParam);
 }
